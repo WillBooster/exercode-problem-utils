605e7f67172917bc1b40cc7a31180351d1e7c4c1
remin <noreply@remin.jp>
2025-11-21 14:15:14 +0900

    fix: rename the properties in a test case

--- src/helpers/readTestCases.ts
+++ src/helpers/readTestCases.ts
@@ -1,10 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-export async function readTestCases(directory: string): Promise<{ id: string; stdin?: string; stdout?: string }[]> {
+export async function readTestCases(directory: string): Promise<{ id: string; input?: string; output?: string }[]> {
   const idSet = new Set<string>();
-  const idToStdin = new Map<string, string>();
-  const idToStdout = new Map<string, string>();
+  const idToInput = new Map<string, string>();
+  const idToOutput = new Map<string, string>();
 
   for (const dirent of await fs.promises.readdir(directory, { withFileTypes: true })) {
     if (!dirent.isFile()) continue;
@@ -15,9 +15,9 @@ export async function readTestCases(directory: string): Promise<{ id: string; st
     const text = await fs.promises.readFile(path.join(dirent.parentPath, dirent.name), 'utf8');
 
     idSet.add(name);
-    if (ext === '.in') idToStdin.set(name, text);
-    if (ext === '.out') idToStdout.set(name, text);
+    if (ext === '.in') idToInput.set(name, text);
+    if (ext === '.out') idToOutput.set(name, text);
   }
 
-  return [...idSet].toSorted().map((id) => ({ id, stdin: idToStdin.get(id), stdout: idToStdout.get(id) }));
+  return [...idSet].toSorted().map((id) => ({ id, input: idToInput.get(id), output: idToOutput.get(id) }));
 }
